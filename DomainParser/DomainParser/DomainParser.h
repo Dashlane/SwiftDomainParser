b1b9aa1165555d42df56957f2c1b4aee2d48af2e
Jason Akakpo <jason.akakpo@dashlane.com>
2018-07-19 16:40:10 +0200

    shared target to be carthage friendly

--- DomainParser/DomainParser/DomainParser.h
+++ DomainParser/DomainParser/DomainParser.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Dashlane. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for DomainParser.
 FOUNDATION_EXPORT double DomainParserVersionNumber;
