7f3e93b9c6aae465a0c67cd4da368c13779102bf
battlmonstr <battlmonstr@users.noreply.github.com>
2022-04-08 17:14:56 +0200

    Allow SPM build for iOS 12

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "DomainParser",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v12),
         .macOS(.v10_14)
     ],
     products: [
